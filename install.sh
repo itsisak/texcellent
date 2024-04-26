7c92565e952c2a1124960f8ffe4d74af9aeeef3d
Isak Berg Endresen <isakbendresen@gmail.com>
2024-04-26 14:56:32 +0200

    Update install script

--- install.sh
+++ install.sh
@@ -5,14 +5,13 @@ if [[ -d "$HOME/.texcellent" ]]; then
     cd "$HOME/.texcellent" || exit 1
     git pull || exit 1
 else
-    git clone "https://github.com/itsisak/texcellent.git" "$HOME/.texcellent" || {
-    echo "Install failed"; exit 1 
-    }
+    git clone "https://github.com/itsisak/texcellent.git" "$HOME/.texcellent" || exit 1
     echo "export TEXCELLENT_DIR='$HOME/.texcellent'" >> "$HOME/.zshrc"
+    echo "export PATH=\$PATH:\$HOME/bin" >> "$HOME/.zshrc"
 fi
 
-cat "$HOME/.texcellent/texcellent.sh" > "$HOME/bin/tx" || {
-    echo "Install failed"; exit 1 
-}
+cat "$HOME/.texcellent/texcellent.sh" > "$HOME/bin/tx"
+
+if [[ ! -x "$HOME/bin/tx" ]]; then chmod +x "$HOME/bin/tx"; fi
 
 echo "Install successful!"
